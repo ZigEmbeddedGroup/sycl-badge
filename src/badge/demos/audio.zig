938a40773a1dc3efd49b2f7930396df37e2721ce
Matt Knight <mattnite@proton.me>
2024-05-15 01:39:20 -0700

    fix clock configuration in audio example (#45)

--- src/badge/demos/audio.zig
+++ src/badge/demos/audio.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const microzig = @import("microzig");
 const hal = microzig.hal;
 const gclk = hal.clocks.gclk;
+const mclk = hal.clocks.mclk;
 const timer = hal.timer;
 
 const spkr_en_pin = microzig.board.SPKR_EN;
@@ -16,10 +17,17 @@ pub fn main() !void {
     analog_out_pin.write(.low);
     spkr_en_pin.write(.high);
 
+    mclk.set_apb_mask(.{
+        .TC0 = .enabled,
+        .TC1 = .enabled,
+    });
+
     gclk.enable_generator(.GCLK1, .DFLL, .{
         .divsel = .DIV1,
         .div = 48,
     });
+
+    gclk.set_peripheral_clk_gen(.GCLK_TC0_TC1, .GCLK1);
 
     timer.init();
     while (true) {

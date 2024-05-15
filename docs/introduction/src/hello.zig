240f0a84cfed30a58cd8fb0a6b02ed594537950a
Guillaume Wenzek <gwenzek@users.noreply.github.com>
2024-05-15 16:28:32 +0200

    cleanup

--- docs/introduction/src/hello.zig
+++ docs/introduction/src/hello.zig
@@ -4,12 +4,15 @@ const builtin = @import("builtin");
 
 export fn start() void {}
 
-var scene: enum { intro, game, mnist } = .mnist;
+var scene: enum { mnist } = .mnist;
+
+var step: usize = 0;
 
 export fn update() void {
+    step +%= 1;
     switch (scene) {
-        .intro => scene_intro(),
-        .game => scene_game(),
+        // .intro => scene_intro(),
+        // .game => scene_game(),
         .mnist => scene_mnist(),
     }
 }
@@ -17,11 +20,8 @@ export fn update() void {
 const Img = [28][28]u8;
 const MNIST_DATA = @embedFile("t10k-images.idx3-ubyte.small");
 const n_images = @divExact(MNIST_DATA.len - 16, @sizeOf(Img));
-var step: usize = 0;
-pub fn randomDigit() Img {
-    // var rng = std.rand.DefaultPrng.init(@intCast(std.time.timestamp()));
-    step +%= 1;
 
+pub fn randomDigit() Img {
     const n = @rem(@divTrunc(step, 2), n_images);
     const img: Img = @bitCast(MNIST_DATA[16 + n * @sizeOf(Img) ..][0..@sizeOf(Img)].*);
     return img;
@@ -107,89 +107,10 @@ fn scene_mnist() void {
     }
 }
 
-fn scene_game() void {
-    set_background();
-
-    const title = "TIC-TAC-TOE";
-    cart.text(.{
-        .text_color = .{ .r = 31, .g = 63, .b = 31 },
-        .str = title,
-        .x = @intCast((cart.screen_width - cart.font_width * title.len) / 2),
-        .y = 10,
-    });
-
-    const instructions = "D-PAD + SELECT";
-    cart.text(.{
-        .text_color = .{ .r = 31, .g = 63, .b = 31 },
-        .str = instructions,
-        .x = @intCast((cart.screen_width - cart.font_width * instructions.len) / 2),
-        .y = cart.screen_height - cart.font_height - 10,
-    });
-
-    for (0..3) |y| {
-        for (0..3) |x| {
-            cart.rect(.{
-                .stroke_color = .{ .r = 31, .g = 63, .b = 31 },
-                .fill_color = if (x == selected_x and y == selected_y) .{ .r = 31 / 2, .g = 63 / 2, .b = 31 / 2 } else null,
-                .x = @intCast(cart.screen_width / 2 + x * 19 - 10 * 3),
-                .y = @intCast(cart.screen_height / 2 + y * 19 - 10 * 3),
-                .width = 20,
-                .height = 20,
-            });
-
-            cart.text(.{
-                .text_color = .{ .r = 31, .g = 63, .b = 31 },
-                .str = switch (state[y][x]) {
-                    .x => "X",
-                    .o => "O",
-                    .none => "",
-                },
-                .x = @intCast(cart.screen_width / 2 + x * 19 - 10 * 3 + 7),
-                .y = @intCast(cart.screen_height / 2 + y * 19 - 10 * 3 + 7),
-            });
-        }
-    }
-
-    if (!control_cooldown) {
-        if (cart.controls.left) selected_x -|= 1;
-        if (cart.controls.right and selected_x != 2) selected_x += 1;
-        if (cart.controls.up) selected_y -|= 1;
-        if (cart.controls.down and selected_y != 2) selected_y += 1;
-        if (cart.controls.select and state[selected_y][selected_x] == .none) {
-            state[selected_y][selected_x] = turn;
-            turn = switch (turn) {
-                .x => .o,
-                .o => .x,
-                else => unreachable,
-            };
-
-            if (check_win()) {
-                turn = .x;
-                @memset(@as(*[9]Player, @ptrCast(&state)), .none);
-                scene = .intro;
-                selected_x = 0;
-                selected_y = 0;
-            }
-        }
-    }
-
-    control_cooldown = false;
-    if (cart.controls.left or cart.controls.right or cart.controls.up or cart.controls.down or cart.controls.select) control_cooldown = true;
-}
-
 fn set_background() void {
     @memset(cart.framebuffer, cart.DisplayColor{
         .r = 0,
         .g = 0,
         .b = 0,
     });
-}
-
-fn check_win() bool {
-    for (0..3) |i| {
-        if (state[i][0] != .none and state[i][0] == state[i][1] and state[i][1] == state[i][2]) return true;
-        if (state[0][i] != .none and state[0][i] == state[1][i] and state[1][i] == state[2][i]) return true;
-    }
-
-    return (state[0][0] != .none and state[0][0] == state[1][1] and state[1][1] == state[2][2]) or (state[0][2] != .none and state[0][2] == state[1][1] and state[1][1] == state[2][0]);
 }

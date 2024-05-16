8fe9436b8bdea836b643297251b2865946695774
Jeff Fowler <jeffowler@gmail.com>
2024-05-16 09:32:10 +0200

    static works

--- docs/introduction/src/rule30.zig
+++ docs/introduction/src/rule30.zig
@@ -1,16 +1,61 @@
 const std = @import("std");
 const cart = @import("cart-api");
 
-export fn start() void {}
+const black = cart.DisplayColor{
+    .r = 0,
+    .g = 0,
+    .b = 0,
+};
 
-var pixel: u5 = 0;
-export fn update() void {
+const green = cart.DisplayColor{
+    .r = 0,
+    .g = 63,
+    .b = 0,
+};
+
+const size = cart.screen_width * cart.screen_height;
+
+var currentbuffer: [size]bool = undefined;
+const sw = cart.screen_width;
+
+export fn start() void {
+    const mid = cart.screen_width / 2;
+    @memset(currentbuffer[mid .. mid + 1], true);
+    for (currentbuffer, 0..) |_, i| {
+        if (i <= sw) {
+            continue;
+        } else {
+            const l = currentbuffer[i - sw - 1];
+            const u = currentbuffer[i - sw];
+            const r = currentbuffer[i - sw + 1];
+
+            if (l and u and r) {
+                currentbuffer[i] = false;
+            } else if (l and u and !r) {
+                currentbuffer[i] = false;
+            } else if (l and !u and r) {
+                currentbuffer[i] = false;
+            } else if (l and !u and !r) {
+                currentbuffer[i] = true;
+            } else if (!l and u and r) {
+                currentbuffer[i] = true;
+            } else if (!l and u and !r) {
+                currentbuffer[i] = true;
+            } else if (!l and !u and r) {
+                currentbuffer[i] = true;
+            } else if (!l and !u and !r) {
+                currentbuffer[i] = false;
+            }
+        }
+    }
+
     for (cart.framebuffer, 0..) |_, i| {
-        @memset(cart.framebuffer[i .. i + 1], cart.DisplayColor{
-            .r = pixel,
-            .g = pixel + 1,
-            .b = pixel + 2,
-        });
-        pixel += 1;
+        if (currentbuffer[i]) {
+            @memset(cart.framebuffer[i .. i + 1], green);
+        } else {
+            @memset(cart.framebuffer[i .. i + 1], black);
+        }
     }
 }
+
+export fn update() void {}

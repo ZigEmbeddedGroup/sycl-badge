ab46c346fe1de0e0c32dda4fb9dd62d574df6db5
Aeden McClain <dev@platypro.net>
2024-05-17 18:24:00 +0200

    Implement missing functions for neopixels demo

--- src/badge/demos/neopixels.zig
+++ src/badge/demos/neopixels.zig
@@ -1,6 +1,8 @@
 const cart = @import("cart-api");
 
-pub export fn render() void {
+pub export fn start() void {}
+
+pub export fn update() void {
     cart.neopixels.* = if (cart.controls.a) .{
         .{ .r = 31, .g = 0, .b = 0 },
         .{ .r = 31, .g = 0, .b = 0 },

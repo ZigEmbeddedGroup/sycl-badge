8cd6d8658988caf8c898ae8b384b9336d5c5eef6
Marcus Ramse <marcus@ramse.se>
2024-05-20 01:26:10 +0200

    sim: sharp neoled brighness curve

--- simulator/src/ui/leds.ts
+++ simulator/src/ui/leds.ts
@@ -40,13 +40,17 @@ export class LEDs extends LitElement {
     @state() public neopixels: [number, number, number, number, number] = [0, 0, 0, 0, 0];
     @state() public redLed: boolean = false;
 
+    amp(v: number): number {
+        return Math.log(v * 100 + 1) * 25;
+    }
+
     render () {
         return html`
             <div class="leds">
                 <div class="neopixels">
                     ${this.neopixels.map(_ => {
                         const [r, g, b] = unpack888(_);
-                        return html`<div class="led-wrapper"><div class="led" style="--color: rgb(${r}, ${g}, ${b})"></div></div>`;
+                        return html`<div class="led-wrapper"><div class="led" style="--color: rgb(${this.amp(r)}, ${this.amp(g)}, ${this.amp(b)})"></div></div>`;
                     })}
                 </div>
                 <div class="led-wrapper"><div class="led" style="--color: rgba(255, 0, 0, ${this.redLed ? 1 : 0.4});"></div></div>

bdbc7d4f946987b59de690cb72cf142f6a5ae920
IvanCh3ung <56063598+IvanCh3ung@users.noreply.github.com>
2025-11-13 18:00:17 -0800

    multicore

--- src/os/system/multicore.zig
+++ src/os/system/multicore.zig
@@ -2,7 +2,7 @@
 const std = @import("std");
 const microzig = @import("microzig");
 const core1 = @import("../core1.zig");
-
+const hal = microzig.hal;
 const interrupt = microzig.interrupts;
 const peripherals = microzig.chip.peripherals;
 
@@ -10,3 +10,15 @@ const CriticalSection = interrupt.CriticalSection;
 const SIO = peripherals.SIO;
 const PSM = peripherals.PSM;
 const PPB = peripherals.PPB;
+var core1_stack: [128]u32 = undefined;
+extern const _external_interrupt_table: usize; // For riscv only
+
+pub const fifo = hal.multicore.fifo;
+
+pub fn launch_core1(entrypoint: *const fn () void) void {
+    hal.multicore.launch_with_stack(entrypoint, &core1_stack);
+}
+
+pub fn launch_core1_with_stack(entrypoint: *const fn () void, stack: []u32) void {
+    hal.multicore.launch_with_stack(entrypoint, stack);
+}

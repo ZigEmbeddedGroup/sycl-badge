d71cf82a4fbca6a1967dbe98471b30e8d2707223
IvanCh3ung <56063598+IvanCh3ung@users.noreply.github.com>
2025-11-13 16:40:46 -0800

    spinlock and multicore

--- src/os/system/multicore.zig
+++ src/os/system/multicore.zig
@@ -2,3 +2,11 @@
 const std = @import("std");
 const microzig = @import("microzig");
 const core1 = @import("../core1.zig");
+
+const interrupt = microzig.interrupts;
+const peripherals = microzig.chip.peripherals;
+
+const CriticalSection = interrupt.CriticalSection;
+const SIO = peripherals.SIO;
+const PSM = peripherals.PSM;
+const PPB = peripherals.PPB;
